dd5fc69ee07b888df8cfd65ad69f715c80443b54
Narahari1994 <narahariv94@gmail.com>
2024-02-12 11:54:11 +0530

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,26 @@
+import menuImg from "./assets/menu.png";
+import tourImg from "./assets/tours.png";
+import reviewsImg from "./assets/reviews.png";
+
+const projects = [
+  {
+    id: Date.now(),
+    title: "menu",
+    url: "https://menu-app-b45f2e.netlify.app/",
+    image: menuImg,
+  },
+  {
+    id: Date.now(),
+    title: "tours",
+    url: "https://tours-app-using-react.netlify.app/",
+    image: tourImg,
+  },
+  {
+    id: Date.now(),
+    title: "reviews",
+    url: "https://reviews-application1567.netlify.app/",
+    image: reviewsImg,
+  },
+];
+
+export default projects;
